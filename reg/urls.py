f9bdc942ef902a56e0ef1c2ad392c6ca501a84c0
AlmazKrasavchik <askerbekuulualmazbek@gmail.com>
2022-12-04 23:51:42 +0600

    update

--- reg/urls.py
+++ reg/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from reg.views import RegisterView, LoginAPIView, ProfileViewSet
 from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 router = DefaultRouter()
 router.register(r'profile', ProfileViewSet, "profile_api")
@@ -9,4 +12,4 @@ urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
     path("", include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
