3106d5d2c190940de56c371e98912f90bde82624
AlmazKrasavchik <askerbekuulualmazbek@gmail.com>
2022-12-09 15:30:36 +0600

    models updated

--- menu/models.py
+++ menu/models.py
@@ -5,27 +5,44 @@ WEEK_CHOICES = ((day, day) for day in WEEK_LIST)
 CATEGORY_LIST = ["breakfast", "lunch", "snack", "dinner"]
 CATEGORY_CHOICES = ((category, category) for category in CATEGORY_LIST)
 
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255, choices=CATEGORY_CHOICES)
+
+    def __str__(self) -> str:
+        return self.category_name
+
 class Fit(models.Model):
     image = models.ImageField()
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=20)
     description = models.TextField()
-    kcal = models.PositiveSmallIntegerField()
+    kcal = models.PositiveIntegerField()
     price = models.PositiveIntegerField()
-    
     def __str__(self) -> str:
         return self.title
-
 
 class Food(models.Model):
-    title = models.CharField(max_length=255, unique=True)
+    image = models.ImageField()
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, choices=CATEGORY_CHOICES)
     calories = models.PositiveSmallIntegerField()
-    squirrels = models.FloatField()
     carbohydrates = models.PositiveSmallIntegerField()
+    squirrels = models.FloatField()
     fats = models.FloatField()
     ingredients = models.TextField()
-    category = models.CharField(max_length=255, choices=CATEGORY_CHOICES, null=True)
-    fit = models.ForeignKey(Fit, on_delete=models.PROTECT, null=True, related_name="food")
-    day = models.CharField(max_length=255, choices=WEEK_CHOICES, null=True)
 
     def __str__(self) -> str:
         return self.title
+
+FOOD_LIST = Food.objects.all()
+FOOD_CHOICES = [(food.id, food.title) for food in FOOD_LIST]
+
+
+class Set(models.Model):
+    day = models.CharField(max_length=255, choices=WEEK_CHOICES)
+    breakfast = models.CharField(max_length=255, choices=FOOD_CHOICES)
+    lunch = models.CharField(max_length=255, choices=FOOD_CHOICES)
+    snack = models.CharField(max_length=255, choices=FOOD_CHOICES)
+    dinner = models.CharField(max_length=255, choices=FOOD_CHOICES)
+    fit = models.ForeignKey(Fit, on_delete=models.PROTECT)
